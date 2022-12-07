9f5bc23a64278850c0619506aa5f8a9ac4fdef46
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2022-12-07 19:53:11 +0800

    desugar test

--- android/src/org/ryuu/learndesugar/AndroidLauncher.java
+++ android/src/org/ryuu/learndesugar/AndroidLauncher.java
@@ -2,15 +2,57 @@ package org.ryuu.learndesugar;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.ryuu.functional.Action;
+import org.ryuu.functional.IAction;
 import org.ryuu.learndesugar.LearnDesugar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new LearnDesugar(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        LearnDesugar learnDesugar = new LearnDesugar();
+        initialize(learnDesugar, config);
+        IAction desugarFalse = () -> {
+            List<Integer> integers = new ArrayList<>();
+            integers.add(1);
+            integers.add(2);
+            integers.add(3);
+            integers.add(4);
+            integers.add(5);
+
+            List<Integer> res = new ArrayList<>();
+
+            for (int i : integers) {
+                if (i <= 3) {
+                    continue;
+                }
+                res.add(i);
+            }
+
+            Gdx.app.log(LearnDesugar.class.getSimpleName(), Arrays.toString(res.toArray()));
+        };
+        IAction desugarTrue = () -> {
+            List<Integer> integers = new ArrayList<>();
+            integers.add(1);
+            integers.add(2);
+            integers.add(3);
+            integers.add(4);
+            integers.add(5);
+
+            List<Integer> collect = integers.stream().filter(integer -> integer > 3).collect(Collectors.toList());
+
+            Gdx.app.log(LearnDesugar.class.getSimpleName(), Arrays.toString(collect.toArray()));
+        };
+        learnDesugar.getAfterCreate().add(desugarFalse);
+        learnDesugar.getAfterCreate().add(desugarTrue);
+    }
 }
--- core/src/org/ryuu/learndesugar/LearnDesugar.java
+++ core/src/org/ryuu/learndesugar/LearnDesugar.java
@@ -5,38 +5,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import lombok.Getter;
+import org.ryuu.functional.Action;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LearnDesugar extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+    @Getter
+    private final Action afterCreate = new Action();
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.add(1);
-        integers.add(2);
-        integers.add(3);
-        integers.add(4);
-        integers.add(5);
-        Gdx.app.log(LearnDesugar.class.getSimpleName(), Arrays.toString(integers.stream().filter(integer -> integer > 0).toArray()));
+        afterCreate.invoke();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        ScreenUtils.clear(0, 0, 0, 1);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
     }
 }
